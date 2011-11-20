e8cdf691f8820aa6a490a1ec66dc02c9731b6477
Tim Garvin <timg@tim10v.(none)>
2011-11-20 00:15:27 -0500

    Initial Commit (Basic Pyramid Scheme)

---
+++ README.txt
@@ -0,0 +1,1 @@
+hwebapp README
---
+++ hwebapp/__init__.py
@@ -0,0 +1,12 @@
+from pyramid.config import Configurator
+from hwebapp.resources import Root
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(root_factory=Root, settings=settings)
+    config.add_view('hwebapp.views.my_view',
+                    context='hwebapp:resources.Root',
+                    renderer='hwebapp:templates/mytemplate.pt')
+    config.add_static_view('static', 'hwebapp:static', cache_max_age=3600)
+    return config.make_wsgi_app()
