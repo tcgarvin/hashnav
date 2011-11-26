e3754bd99a3fc62f669895aded46e7e2af62c6c7
Tim Garvin <tcgarvin@gmail.com>
2011-11-26 02:35:38 -0500

    Added a mutation of graphExample, and made it the default page.

--- hwebapp/__init__.py
+++ hwebapp/__init__.py
@@ -7,6 +7,6 @@ def main(global_config, **settings):
     config = Configurator(root_factory=Root, settings=settings)
     config.add_view('hwebapp.views.my_view',
                     context='hwebapp:resources.Root',
-                    renderer='hwebapp:templates/graphExample.pt')
+                    renderer='hwebapp:templates/staticData.pt')
     config.add_static_view('static', 'hwebapp:static', cache_max_age=3600)
     return config.make_wsgi_app()
